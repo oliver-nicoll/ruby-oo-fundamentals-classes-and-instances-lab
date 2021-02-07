ce18e15f0ec872a13685204bf3db2fd6b3ada0bf
Nicoll Oliver <nicolldon96@gmail.com>
2021-02-07 14:11:41 -0500

    commit tree b8e25a9e038d5a6782e1097df1f4f777cb94745f

---
+++ lib/dog.rb
@@ -0,0 +1,10 @@
+class Dog
+    #code here
+end
+
+
+fido = Dog.new
+
+snoopy = Dog.new
+
+lassie = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,7 @@
+class Person
+    #code here
+end
+
+adele_goldberg = Person.new
+
+alan_kay = Person.new
